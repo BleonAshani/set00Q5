13c5ba3f4e1ecf4bd3cdf507dec8051813abe481
Bleon Ashani <Bleon.Ashani@students.ittralee.ie>
2019-09-19 12:50:25 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String operation;
+
+        operation = JOptionPane.showInputDialog(null,"Pick one of the operation desired" +
+                                    "A (for addition) \n" +
+                                    "S (for subtraction) \n" +
+                                    "M (for multiplication) \n" +
+                                    "D (for division) \n " );
+
+
+        switch(operation){
+            case 'A':
+
+        }
+    }
+}
